37873ce93f61ba1f1ad8d7db9701a88896ada137
Даниил Сергин <99390763+ChirpaEwok@users.noreply.github.com>
2022-06-08 16:37:54 +0300

    Update move.c

--- functions/move.c
+++ functions/move.c
@@ -13,19 +13,23 @@ void move(text txt, int m, int n)
     node *current = txt->begin;
     while(current)
     {
-	if (cpis == m)     
-	    break;
-	current = current->next;
-	cpis++;
+		if (cpis == m)     
+	    	break;
+		current = current->next;
+		cpis++;
     }
     txt->cursor->line = current;
     int currentstrlen = (int)strlen(current->contents) - 1;
     if (n <= currentstrlen)
     {
-	txt->cursor->position = n;
+		txt->cursor->position = n;
     }
     else
     {
-	txt->cursor->position = currentstrlen;
+		txt->cursor->position = currentstrlen;
     }
+	if (n < 0)
+	{
+		txt->cursor->position = 0;	
+	}
 }
