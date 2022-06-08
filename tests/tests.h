c9e6cefc6775dc547a03acf521939d240f183625
Даниил Сергин <99390763+ChirpaEwok@users.noreply.github.com>
2022-06-08 20:36:22 +0300

    Update tests.h

--- tests/tests.h
+++ tests/tests.h
@@ -34,7 +34,7 @@ TEST(load, nonexistent_file)
 {
     text txt = create_text();
     std::string output;
-    GetErr(load(txt, "nothing.txt"););
+    GetErr(load(txt, "nothing.txt");)
     ASSERT_EQ(output, "The file nothing.txt cannot be opened\n");
 }
 
@@ -66,7 +66,7 @@ TEST(save, save_to_not_writable_file)
 {
     text txt = txt_load(inputfile);
     std::string output;	
-    GetErr(save(txt, "usr/lib"););
+    GetErr(save(txt, "usr/lib");)
     ASSERT_EQ(output, "The file usr/lib cannot be written\n");
     remove_all(txt);
 }
@@ -75,7 +75,7 @@ TEST(save, nothing_to_save)
 {
     text txt = create_text();
     std::string output;	
-    GetOutput(save(txt, "output.txt"););
+    GetOutput(save(txt, "output.txt");)
     ASSERT_EQ(output, "Nothing to save\n");
     remove_all(txt);
 }
@@ -103,19 +103,19 @@ TEST(move, correct_moving)
 	text txt = txt_load(inputfile);
 	std::string output;
 	move(txt, 1, 1);
-	GetOutput(show(txt););
+	GetOutput(show(txt);)
 	ASSERT_EQ(output, "1234\nq|wer\nasdf\nz x c v\n");
 	move(txt, 0, 10);
-	GetOutput(show(txt););
+	GetOutput(show(txt);)
 	ASSERT_EQ(output, "1234|\nqwer\nasdf\nz x c v\n");
 	move(txt, 2, -10);
-	GetOutput(show(txt););
+	GetOutput(show(txt);)
 	ASSERT_EQ(output, "1234\nqwer\n|asdf\nz x c v\n");
 	move(txt, -1, 1);
-	GetOutput(show(txt););
+	GetOutput(show(txt);)
 	ASSERT_EQ(output, "1|234\nqwer\nasdf\nz x c v\n");
 	move(txt, 4, 1);
-	GetOutput(show(txt););
+	GetOutput(show(txt);)
 	ASSERT_EQ(output, "1234\nqwer\nasdf\nz| x c v\n");
 	remove_all(txt);
 }
@@ -124,7 +124,7 @@ TEST(showwordbeginnings, correct_showing)
 {
 	text txt = txt_load(inputfile);
 	std::string output;
-	GetOutput(showwordbeginnings(txt););
+	GetOutput(showwordbeginnings(txt);)
 	ASSERT_EQ(output, "1\nq\na\nzxcv\n");
 	remove_all(txt);
 }
@@ -133,7 +133,7 @@ TEST(showwordbeginnings, show_empty)
 {
 	text txt = create_text();
 	std::string output;
-	GetErr(showwordbeginnings(txt););
+	GetErr(showwordbeginnings(txt);)
 	ASSERT_EQ(output, "There are already no any lines in the text!\n");
     remove_all(txt);
 }
@@ -143,7 +143,7 @@ TEST(y, correct_replacment)
 	text txt = txt_load(inputfile);
 	std::string output;
 	move(txt, 1, 1);
-	GetOutput(y(txt, "rt"););
+	GetOutput(y(txt, "rt");)
 	ASSERT_EQ(output, "1234\nq|rt\nasdf\nz x c v\n");
 	GetOutput(y(txt, "werty"););
 	ASSERT_EQ(output, "1234\nq|werty\nasdf\nz x c v\n");
@@ -158,7 +158,7 @@ TEST(y, overflow)
 	std::string output;
 	move(txt, 1, 1);
 	GetErr(y(txt, "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"););
-	ASSERT_EQ(output, "Too many letters\n");
+	ASSERT_EQ(output, "Too many letters\n")
 	remove_all(txt);
 }
 
@@ -167,9 +167,9 @@ TEST(ce, correct_working)
 	text txt = txt_load(inputfile);
 	std::string output;
 	move(txt, 1, 1);
-	GetOutput(ce(txt););
+	GetOutput(ce(txt);)
 	ASSERT_EQ(output, "1234\nasdf\nz x c v\nq|wer\n");
-	GetOutput(ce(txt););
+	GetOutput(ce(txt);)
 	ASSERT_EQ(output, "1234\nasdf\nz x c v\nq|wer\n");
 	remove_all(txt);
 }
